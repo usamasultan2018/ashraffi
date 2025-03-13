cabbdc8f1b9ce03a38a5c683b6fae63dc4358c55
usamasultan2018 <usama.khan2018@gmail.com>
2025-03-13 13:06:56 +0500

    Fix case sensitivity issue

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,7 +10,7 @@ const {
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/verify-otp", verifyOtp);
+router.post("/verify-otpp", verifyOtp);
 router.post("/resend-otp", resendOtp);
 router.post("/login", loginUser);
 router.post("/forgot-password", forgotPassword);
